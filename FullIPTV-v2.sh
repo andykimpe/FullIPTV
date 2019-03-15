e52528f73411cb2101e29bb24f33562492f3dbfe
Kimpe Andy <andykimpe@gmail.com>
2019-03-15 17:49:33 +0100

    Update FullIPTV-v2.sh

--- FullIPTV-v2.sh
+++ FullIPTV-v2.sh
@@ -10,9 +10,45 @@
 
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8  >> /dev/null 2>&1
+export LANG="en_US.UTF-8" >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get update -y -q >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -q dist-upgrade >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get update -y -q >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -d install software-properties-common >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -d install python-software-properties >> /dev/null 2>&1	
+LANG=en_US.UTF-8 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php >> /dev/null 2>&1
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRIB_CODENAME-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get update -y -q >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install libjansson-dev -q -y --force-yes >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes -q install lsb-release apt-utils aptitude apt software-properties-common curl mtr debconf html2text wget whois whiptail vim-nox unzip tzdata sudo sysstat strace sshpass ssh-import-id tcpdump telnet screen python-software-properties python openssl ntpdate mc iptraf mailutils mlocate mtr htop gcc fuse ftp dnsutils ethtool curl dbconfig-common coreutils debianutils debconf bc bash-completion automake autoconf bwm-ng apt-utils aptitude apt git software-properties-common dos2unix dialog curl >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install postgresql postgresql-9.3 -y -q >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -q install php5.6-fpm php5.6-mcrypt php5.6-pgsql php5.6-cli php5.6-curl php5.6-gd php-pear libssh2-php5.6 libssh2-php php5.6 php5.6-dev libapache2-mod-php5.6 php5.6-mcrypt whois >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -q install php5.6-fpm php5.6-mcrypt php5.6-pgsql php5.6-cli php5.6-curl php5.6-gd php-pear libssh2-php5.6 libssh2-php php5.6 php5.6-dev libapache2-mod-php5.6 php5.6-mcrypt whois >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -q install php5.6-fpm php5.6-mcrypt php5.6-pgsql php5.6-cli php5.6-curl php5.6-gd php-pear libssh2-php5.6 libssh2-php php5.6 php5.6-dev libapache2-mod-php5.6 php5.6-mcrypt whois >> /dev/null 2>&1
+LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y -q install php5.6-fpm php5.6-mcrypt php5.6-pgsql php5.6-cli php5.6-curl php5.6-gd php-pear libssh2-php5.6 libssh2-php php5.6 php5.6-dev libapache2-mod-php5.6 php5.6-mcrypt whois >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php7.3 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php7.2 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php7.1 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php7.0 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php7 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php5 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2dismod php5.6 >> /dev/null 2>&1
+LANG=en_US.UTF-8 a2enmod php5.6 >> /dev/null 2>&1
+LANG=en_US.UTF-8 service apache2 restart >> /dev/null 2>&1
+LANG=en_US.UTF-8 update-alternatives --set php /usr/bin/php5.6 >> /dev/null 2>&1
+LANG=en_US.UTF-8 update-alternatives --set phar /usr/bin/phar5.5 >> /dev/null 2>&1
+LANG=en_US.UTF-8 update-alternatives --set phar.phar /usr/bin/phar.phar5.5 >> /dev/null 2>&1
+LANG=en_US.UTF-8 update-alternatives --set phpize /usr/bin/phpize5.5 >> /dev/null 2>&1
+LANG=en_US.UTF-8 update-alternatives --set php-config /usr/bin/php-config5.6 >> /dev/null 2>&1
+LANG=en_US.UTF-8 update-alternatives --set php-config /usr/bin/php-config5.6 >> /dev/null 2>&1
+		
 
-apt-get update && apt-get upgrade -y
-apt-get install postgresql -y
 
 pg_createcluster 9.3 main --start 
 /etc/init.d/postgresql start
@@ -134,7 +170,7 @@ function installBase {
 	LANG=en_US.UTF-8 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php >> /dev/null 2>&1
 	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
 	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get update -y -q >> /dev/null 2>&1
-        LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install libjansson-dev -q -y --force-yes >> /dev/null 2>&1
+    LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install libjansson-dev -q -y --force-yes >> /dev/null 2>&1
 	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes -q install lsb-release apt-utils aptitude apt software-properties-common curl mtr debconf html2text wget whois whiptail vim-nox unzip tzdata sudo sysstat strace sshpass ssh-import-id tcpdump telnet screen python-software-properties python openssl ntpdate mc iptraf mailutils mlocate mtr htop gcc fuse ftp dnsutils ethtool curl dbconfig-common coreutils debianutils debconf bc bash-completion automake autoconf bwm-ng apt-utils aptitude apt git software-properties-common dos2unix dialog curl >> /dev/null 2>&1
 	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
 	
@@ -216,7 +252,7 @@ function upgradeFiles {
 function installCMSPackages {
 	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive dpkg --configure -a >> /dev/null 2>&1
 	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install php5.6-fpm php5.6-mcrypt php5.6-pgsql php5.6-cli php5.6-curl php5.6-gd php-pear libssh2-php5.6 libssh2-php php5.6-json libxslt1.1 daemontools postgresql-client -q -y --force-yes >> /dev/null 2>&1
-	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install libva1 libxfixes3 libxext6 libasound2 libsdl1.2debian libtheora0 libmp3lame0 libass4 libvdpau1 daemontools postgresql-client apache2 php5.6 php5.6-dev libapache2-mod-php5.6 php5.6-mcrypt  whois-q -y --force-yes >> /dev/null 2>&1
+	LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install libva1 libxfixes3 libxext6 libasound2 libsdl1.2debian libtheora0 libmp3lame0 libass4 libvdpau1 daemontools postgresql-client apache2 php5.6-fpm php5.6-mcrypt php5.6-pgsql php5.6-cli php5.6-curl php5.6-gd php-pear libssh2-php5.6 libssh2-php php5.6 php5.6-dev libapache2-mod-php5.6 php5.6-mcrypt  whois-q -y --force-yes >> /dev/null 2>&1
 	LANG=en_US.UTF-8 a2dismod php7.3 >> /dev/null 2>&1
 	LANG=en_US.UTF-8 a2dismod php7.2 >> /dev/null 2>&1
 	LANG=en_US.UTF-8 a2dismod php7.1 >> /dev/null 2>&1
@@ -241,7 +277,7 @@ function installCMSPackages {
 		echo -e "listen_addresses = '*'\n" >> /etc/postgresql/9.3/main/postgresql.conf
 	else
 		echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRIB_CODENAME-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-		wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+		wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - >> /dev/null 2>&1
 		LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get update -q >> /dev/null 2>&1
 		LANG=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install postgresql-9.3 -y --force-yes >> /dev/null 2>&1
 		echo -e "local all postgres trust\n" > /etc/postgresql/9.3/main/pg_hba.conf
